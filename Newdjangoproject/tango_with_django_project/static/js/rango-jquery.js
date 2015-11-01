aef9ee80cce4dd17442de86e127e89ce93801abe
eddy939 <eddyonyango@gmail.com>
2015-11-01 20:59:59 +0300

    end of chapter 19 rango

---
+++ Newdjangoproject/tango_with_django_project/static/js/rango-jquery.js
@@ -0,0 +1,36 @@
+$(document).ready( function() {
+
+    $("#about-btn").click( function(event) {
+        alert("You clicked the button using JQuery!");
+    });
+
+    $("#about-btn").addClass('btn btn-primary')
+
+    $("#about-btn2").addClass('btn btn-primary')
+
+
+    $(".ouch").click(function(event) {
+                alert("You clicked me! ouch!");
+    });
+
+
+    $("p").hover(function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+                $(this).css('color', 'blue');
+        });
+
+
+    $("#about-btn").click(function(event) {
+    msgstr = $("#msg").html()
+            msgstr = msgstr + "o"
+            $("#msg").html(msgstr)
+    });
+
+    $("#category-btn").addClass('btn btn-primary')
+
+
+
+
+});
